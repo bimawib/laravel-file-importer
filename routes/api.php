005619bf2aa711f940f5c9891d48d3b6143dca47
Bima Wibowo <bima.wibowo@jejakin.com>
2025-11-09 12:01:46 +0700

    feat(route): add all newly created function related to register, role, permission and user to the api routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PermissionController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,9 +18,23 @@ use App\Http\Controllers\AuthController;
 |
 */
 
+Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware(['jwt'])->group(function () {
     Route::get('/login-information', [AuthController::class, 'userInfo']);
     Route::post('/logout', [AuthController::class, 'logout']);
+}); 
+
+Route::middleware(['jwt', 'permission:users:manage'])->prefix('admin')->group(function () {
+    Route::get('/permissions', [PermissionController::class, 'listPermissions']);
+    Route::post('/permissions', [PermissionController::class, 'createPermission']);
+    Route::put('/permissions/{permission}', [PermissionController::class, 'updatePermission']);
+
+    Route::get('/roles', [RoleController::class, 'listRoles']);
+    Route::post('/roles', [RoleController::class, 'createRole']);
+    Route::put('/roles/{role}', [RoleController::class, 'updateRole']);
+
+    Route::post('/roles/assign', [RoleController::class, 'assignRoleToUser']);
+    Route::get('/users', [UserController::class, 'listUsers']);
 });
